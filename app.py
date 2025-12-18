a5d23df5b517bed8f7fb9ef627dd2500f8fbd11d
Cynthia <kim.sosa2202@gmail.com>
2025-12-18 13:29:07 -0500

    Add checklist feature with multiple chart displays

--- app.py
+++ app.py
@@ -11,72 +11,123 @@ URL = "https://raw.githubusercontent.com/plotly/datasets/refs/heads/master/2014_
 cities_df = pd.read_csv(URL)
 cities_df.sort_values(by='pop', ascending=False, inplace=True)
 
-# Get top 5 and bottom 5 cities
-top_5_df = cities_df.head(5)
-bottom_5_df = cities_df.tail(5)
-
 # App layout
 app.layout = html.Div([
     html.H1("US Cities Population Dashboard (2014)", 
             style={'textAlign': 'center', 'color': '#2c3e50', 'marginBottom': 30}),
     
     html.Div([
-        html.Label("Select Dataset:", 
-                   style={'fontSize': 18, 'fontWeight': 'bold', 'marginRight': 10}),
-        dcc.Dropdown(
-            id='dataset-dropdown',
+        html.Label("Select Display Options:", 
+                   style={'fontSize': 18, 'fontWeight': 'bold', 'marginBottom': 10}),
+        dcc.Checklist(
+            id='display-checklist',
             options=[
-                {'label': 'Top 5 Most Populous Cities', 'value': 'top'},
-                {'label': 'Bottom 5 Least Populous Cities', 'value': 'bottom'}
+                {'label': ' Show top five cities by population', 'value': 'top5'},
+                {'label': ' Show bottom five cities by population', 'value': 'bottom5'},
+                {'label': ' Show top 10 cities by population', 'value': 'top10'}
             ],
-            value='top',
-            style={'width': '400px'}
+            value=['top5'],  # Default selection
+            style={'fontSize': 16},
+            labelStyle={'display': 'block', 'marginBottom': 10}
         )
-    ], style={'marginBottom': 30, 'display': 'flex', 'alignItems': 'center', 'justifyContent': 'center'}),
+    ], style={
+        'marginBottom': 30, 
+        'padding': '20px',
+        'backgroundColor': '#f8f9fa',
+        'borderRadius': '10px',
+        'maxWidth': '500px',
+        'margin': '0 auto 30px auto'
+    }),
     
-    html.Div([
-        dcc.Graph(id='population-chart')
-    ], style={'padding': '20px'})
+    html.Div(id='charts-container', style={'padding': '20px'})
+    
 ], style={'fontFamily': 'Arial, sans-serif', 'padding': '20px'})
 
-# Callback to update the chart based on dropdown selection
+# Callback to update the charts based on checklist selection
 @app.callback(
-    Output('population-chart', 'figure'),
-    Input('dataset-dropdown', 'value')
+    Output('charts-container', 'children'),
+    Input('display-checklist', 'value')
 )
-def update_chart(selected_dataset):
-    if selected_dataset == 'top':
-        df = top_5_df
-        title = "Top 5 Most Populous US Cities (2014)"
-        color = '#3498db'
-    else:
-        df = bottom_5_df
-        title = "Bottom 5 Least Populous US Cities (2014)"
-        color = '#e74c3c'
+def update_charts(selected_options):
+    charts = []
     
-    fig = px.bar(
-        df, 
-        x='name', 
-        y='pop',
-        title=title,
-        labels={'name': 'City', 'pop': 'Population'},
-        color_discrete_sequence=[color]
-    )
+    if not selected_options:
+        return html.Div("Please select at least one option above.", 
+                       style={'textAlign': 'center', 'fontSize': 18, 'color': '#7f8c8d'})
     
-    fig.update_layout(
-        xaxis_title="City Name",
-        yaxis_title="Population",
-        title_x=0.5,
-        title_font_size=20,
-        showlegend=False,
-        hovermode='x'
-    )
+    # Show top 5 cities
+    if 'top5' in selected_options:
+        top_5_df = cities_df.head(5)
+        fig_top5 = px.bar(
+            top_5_df, 
+            x='name', 
+            y='pop',
+            title="Top 5 Most Populous US Cities (2014)",
+            labels={'name': 'City', 'pop': 'Population'},
+            color_discrete_sequence=['#3498db']
+        )
+        fig_top5.update_layout(
+            xaxis_title="City Name",
+            yaxis_title="Population",
+            title_x=0.5,
+            title_font_size=20,
+            showlegend=False,
+            hovermode='x'
+        )
+        fig_top5.update_traces(
+            hovertemplate='<b>%{x}</b><br>Population: %{y:,}<extra></extra>'
+        )
+        charts.append(dcc.Graph(figure=fig_top5, style={'marginBottom': 30}))
     
-    fig.update_traces(
-        hovertemplate='<b>%{x}</b><br>Population: %{y:,}<extra></extra>'
-    )
+    # Show bottom 5 cities
+    if 'bottom5' in selected_options:
+        bottom_5_df = cities_df.tail(5)
+        fig_bottom5 = px.bar(
+            bottom_5_df, 
+            x='name', 
+            y='pop',
+            title="Bottom 5 Least Populous US Cities (2014)",
+            labels={'name': 'City', 'pop': 'Population'},
+            color_discrete_sequence=['#e74c3c']
+        )
+        fig_bottom5.update_layout(
+            xaxis_title="City Name",
+            yaxis_title="Population",
+            title_x=0.5,
+            title_font_size=20,
+            showlegend=False,
+            hovermode='x'
+        )
+        fig_bottom5.update_traces(
+            hovertemplate='<b>%{x}</b><br>Population: %{y:,}<extra></extra>'
+        )
+        charts.append(dcc.Graph(figure=fig_bottom5, style={'marginBottom': 30}))
     
-    return fig
+    # Show top 10 cities
+    if 'top10' in selected_options:
+        top_10_df = cities_df.head(10)
+        fig_top10 = px.bar(
+            top_10_df, 
+            x='name', 
+            y='pop',
+            title="Top 10 Most Populous US Cities (2014)",
+            labels={'name': 'City', 'pop': 'Population'},
+            color_discrete_sequence=['#2ecc71']
+        )
+        fig_top10.update_layout(
+            xaxis_title="City Name",
+            yaxis_title="Population",
+            title_x=0.5,
+            title_font_size=20,
+            showlegend=False,
+            hovermode='x'
+        )
+        fig_top10.update_traces(
+            hovertemplate='<b>%{x}</b><br>Population: %{y:,}<extra></extra>'
+        )
+        charts.append(dcc.Graph(figure=fig_top10, style={'marginBottom': 30}))
+    
+    return charts
 
 if __name__ == '__main__':
     app.run_server(debug=True)
